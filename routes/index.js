055abc2a488cb17b94b04b4e4def22f6761c0bed
me <k89jy@naver.com>
2020-08-01 17:36:27 +0900

    fix routes_name

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/2', function(req, res, next) {
-  // res.render("<html></html>",{title:"title"});
+router.get('/sendPage', function(req, res, next) {
+  
   res.render("main.html");
 });
+
+router.get('/firstPage', function(req, res, next) {
+  console.log('first_paget render : ')
+  res.render('firstPage.html');
+});
+
 
 module.exports = router;
