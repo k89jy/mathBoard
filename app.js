3baf685cc788e676e3a469a90caac070f3eca860
me <k89jy@naver.com>
2020-08-11 17:21:26 +0900

    add css

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-
 var ejs = require('ejs')
 var logger = require('morgan');
+
 
 var indexRouter = require('./routes/index');
 
@@ -14,25 +14,25 @@ var app = express();
 //view engine setup
 app.set('views', path.join(__dirname, '/views'));
 app.engine('html', ejs.renderFile);
-app.set('view engine', 'ejs');
+//app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static('views'));
-app.use(express.static(path.join(__dirname, 'public/html')));
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '/views')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/', indexRouter);
 
 
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
